da1df500bf5956ebf512511e6686b182293e4300
Marwah Al-Dujaili <marwah@aldujaili.com>
2024-01-30 14:21:44 +0100

    admin email confirmation done

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,12 +4,14 @@ import {
   registerNewAdmin,
   editAdmin,
   changeAdminPassword,
+  getConfirmation,
 } from "../controllers/adminController.js";
 import { authenticateAdmin } from "../middleware/authenticateAdmin.js";
 
 const router = express.Router();
 
 router.post("/newadmin", registerNewAdmin);
+router.get("/confirm/:token", getConfirmation);
 router.get("/adminlogin", loginAdmin);
 router.put("/edit", authenticateAdmin, editAdmin);
 router.put("/changepassword", authenticateAdmin, changeAdminPassword);
